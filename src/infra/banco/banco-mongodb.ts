8b5eb996a89375eb07e68081517b59896e0de59e
pamela-goncalvess <pamela.goncalves@estudante.ifms.edu.br>
2023-11-14 08:28:12 -0400

    Commit 14/11 - primeira aula

--- src/infra/banco/banco-mongodb.ts
+++ src/infra/banco/banco-mongodb.ts
@@ -1,39 +1,40 @@
 import mongoose from "mongoose";
 import FilmeRepositorioInterface from "../../aplicacao/filme-repositorio-interface";
 require("dotenv").config();
-export class BancoMongoDB implements  FilmeRepositorioInterface {
-  public filmeModelo:any;
+export default class BancoMongoDB implements  FilmeRepositorioInterface {
+  public filmeModelo: any;
   constructor() {
     try{
-    mongoose.connect(process.env.MONGODB_URL || "")
-  }catch(error){
+      mongoose.connect(process.env.MONGODB_URL || "")
+    }catch(error){
       console.log(error)
     }
-    this.filmeModelo = mongoose.model("Filme", new mongoose.Schema({
-        _id:Number,
-        titulo: String,
-        descricao: String,
-        imagem: String
-      })
+    this.filmeModelo = mongoose.model("Filme", 
+        new mongoose.Schema({
+            _id: Number,
+            titulo: String,
+            descricao: String,
+            imagem: String
+        })
     )
   }
   async salvar(filme: Filme): Promise<Filme> {
     const filmeDTO = {
-        _id: filme.id,
-        titulo: filme.titulo,
-        descricao: filme.descricao,
-        imagem: filme.imagem
-     }
-     const filmeModelo = new this.filmeModelo(filmeDTO)
-     const result = await filmeModelo.save()
-     const filmeSalvo = {
-        id: result.id,
-        titulo: result.titulo,
-        descricao: result.descricao,
-        imagem: result.imagem
-     }
-     return filmeSalvo
+      _id: filme.id,
+      titulo: filme.titulo,
+      descricao: filme.descricao,
+      imagem: filme.imagem
     }
+    const filmeModelo = new this.filmeModelo(filmeDTO)
+    const result =  await filmeModelo.save()
+    const filmeSalvo = {
+      id: result._id,
+      titulo: result.titulo,
+      descricao: result.descricao,
+      imagem: result.imagem
+    }
+    return filmeSalvo
+  }
   async listar(): Promise<Filme[]> {
     return new Promise((resolve, reject) => {
       reject(new Error("Not implemented yet"));
