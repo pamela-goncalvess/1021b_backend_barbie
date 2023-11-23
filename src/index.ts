354a32b86f734c511062a42b46d57bd2b907c62a
pamela-goncalvess <pamela.goncalves@estudante.ifms.edu.br>
2023-11-23 14:56:02 -0400

    commit -23-12

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,21 @@
 import express from 'express'
-const app = express()
-app.use(express.json())
 import ListaFilme from './aplicacao/lista-filme.use-case'
 import BancoMongoDB from './infra/banco/banco-mongodb'
 import SalvaFilme from './aplicacao/salva-filme.use-case'
+import cors from "cors"
+
+const app = express()
 const bancoMongoDB = new BancoMongoDB()
-app.get('/filmes',async (req,res)=>{
-    //usem o listarFilme Usecase para listar os filmes
-    const listaFilme = new ListaFilme(bancoMongoDB)
-    const filmes = await listaFilme.executar()
-    res.send(filmes)
-})
+app.use(express.json())
+app.use(cors())
+
+type Filme = {
+    id:number,
+    titulo:string,
+    descricao:string,
+    imagem:string
+}
+let filmesCadastros:Filme[] = []
 app.post('/filmes', async(req,res)=>{
     const {id,titulo,descricao,imagem} = req.body
     const filme = {
@@ -19,13 +24,21 @@ app.post('/filmes', async(req,res)=>{
         descricao,
         imagem
     }
-    //Como eu salvo o filme que foi cadastrado no meu vetor de filmes (Banco de dados)
-    const salvaFilme = new SalvaFilme(bancoMongoDB)
+    try{
+       const salvaFilme = new SalvaFilme(bancoMongoDB)
     const filmes = await salvaFilme.execute(filme)
-    
-    ////////////////////////////////
-    filmesCadastros.push(filme)
+    /*filmesCadastros.push(filme)*/
     res.status(201).send(filme)
+}catch(error){
+    res.status(404).send("Filme já adicionado.");
+}
+})
+
+app.get('/filmes',async (req,res)=>{
+    //usem o listarFilme Usecase para listar os filmes
+    const listaFilme = new ListaFilme(bancoMongoDB)
+    const filmes = await listaFilme.executar()
+    res.send(filmes)
 })
 
 app.get('/filmes/:id',(req,res)=>{
@@ -38,14 +51,8 @@ app.get('/filmes/:id',(req,res)=>{
 
 //Tenho que iniciar o servidor na porta 3000
 app.listen(3000,()=>{
-    console.log('Servidor rodando na porta 3000')
+    console.log('Servidor rodando na porta 3000.')
 })
 
 
-type Filme = {
-    id:number,
-    titulo:string,
-    descricao:string,
-    imagem:string
-}
-let filmesCadastros:Filme[] = []
+
